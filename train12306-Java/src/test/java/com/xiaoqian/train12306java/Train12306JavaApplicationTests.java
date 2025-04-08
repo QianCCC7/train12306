9829838fcd30649670950fa3382895223c715a73
xiaoqian <1805521303@qq.com>
2025-04-08 17:50:47 +0800

    后端项目初始化

---
+++ train12306-Java/src/test/java/com/xiaoqian/train12306java/Train12306JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaoqian.train12306java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Train12306JavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
