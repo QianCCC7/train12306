cc7ebb57e8e885debc31875f6fb215821b879e76
xiaoqian <1805521303@qq.com>
2025-04-13 17:57:38 +0800

    后端配置服务启动日志

--- train12306-Java/member/src/main/java/com/xiaoqian/member/config/MemberApplication.java
+++ train12306-Java/member/src/main/java/com/xiaoqian/member/config/MemberApplication.java
@@ -1,15 +1,21 @@
 package com.xiaoqian.member.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("com.xiaoqian")
 public class MemberApplication {
-
+    private final static Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class, args);
+        SpringApplication app = new SpringApplication(MemberApplication.class);
+        Environment environment = app.run(args).getEnvironment();
+        LOG.info("member服务启动成功");
+        LOG.info("地址: http://127.0.0.1:{}", environment.getProperty("server.port"));
     }
 
 }
