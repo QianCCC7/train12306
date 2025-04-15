874b9eb254bd96db6fa79b879273d1f637e91c56
xiaoqian <1805521303@qq.com>
2025-04-15 18:04:56 +0800

    前端增加axios拦截器

--- train12306-web/src/main.js
+++ train12306-web/src/main.js
@@ -3,8 +3,23 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Antd from 'ant-design-vue';
+import axios from "axios";
 
 createApp(App).use(store)
     .use(router)
     .use(Antd)
     .mount('#app')
+
+axios.interceptors.request.use(config => {
+    console.log('请求参数：', config.data);
+    return config;
+}, error => {
+    return Promise.reject(error);
+});
+axios.interceptors.response.use(res => {
+    console.log('返回结果：', res.data);
+    return res;
+}, error => {
+    console.log('返回错误：', error);
+    return Promise.reject(error);
+});
