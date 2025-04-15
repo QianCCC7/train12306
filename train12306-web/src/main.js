5d032f31c46e7abfa971410c5d38a31305606d18
xiaoqian <1805521303@qq.com>
2025-04-15 15:37:58 +0800

    前端实现登录注册页面

--- train12306-web/src/main.js
+++ train12306-web/src/main.js
@@ -1,20 +1,10 @@
-import {createApp, nextTick} from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import Antd from 'ant-design-vue';
-import * as Icons from '@ant-design/icons-vue'
 
-const app = createApp(App)
-app.use(store)
+createApp(App).use(store)
     .use(router)
     .use(Antd)
     .mount('#app')
-
-// 必须使用 nextTick，不然会有加载顺序问题，导致绑定失败
-nextTick(() => {
-    // 配置全局对象
-    app.config.globalProperties.$icons = Icons
-    // Antd 注入全部图标（这样注入之后，就可以全局直接使用 icon 组件，不需要每个页面去引入了）
-    for (const key in Icons) { app.component(key, Icons[key]) }
-})
--- train12306-web/src/views/AboutView.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <a-button>hello</a-button>
-    <a-button type="primary" danger>Primary</a-button>
-  </div>
-</template>
