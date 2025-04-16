1708d8ff88e32789969f76ebc5c82a7199ae5763
xiaoqian <1805521303@qq.com>
2025-04-16 12:06:57 +0800

    后端gateway认证拦截器自定义响应

--- train12306-Java/gateway/src/main/java/com/xiaoqian/gateway/config/AuthorizeFilter.java
+++ train12306-Java/gateway/src/main/java/com/xiaoqian/gateway/config/AuthorizeFilter.java
@@ -1,16 +1,23 @@
 package com.xiaoqian.gateway.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.xiaoqian.gateway.util.JwtUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.annotation.Order;
+import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Order(0)
@@ -33,8 +40,23 @@ public class AuthorizeFilter implements GlobalFilter {
         // 校验token是否有效
         if (!StringUtils.hasText(token) || !JwtUtil.validate(token)) {
             log.error("用户未登录");
-            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);// 设置一个状态码
-            return exchange.getResponse().setComplete();// 拦截
+            // 构造自定义返回体
+            Map<String, Object> data = new HashMap<>();
+            data.put("code", 401);
+            data.put("data", null);
+            data.put("msg", "用户未登录或Token无效");
+            byte[] responseBytes;
+            try {
+                ObjectMapper objectMapper = new ObjectMapper();
+                responseBytes = objectMapper.writeValueAsBytes(data);
+            } catch (Exception e) {
+                responseBytes = "{\"code\":500,\"msg\":\"响应序列化失败\"}".getBytes(StandardCharsets.UTF_8);
+            }
+            // 设置响应头
+            exchange.getResponse().getHeaders().setContentType(MediaType.APPLICATION_JSON);
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED); // 可选
+            DataBuffer buffer = exchange.getResponse().bufferFactory().wrap(responseBytes);
+            return exchange.getResponse().writeWith(Mono.just(buffer)); // 拦截
         }
         log.info("AuthorizeFilter通过登录认证");
         return chain.filter(exchange);
