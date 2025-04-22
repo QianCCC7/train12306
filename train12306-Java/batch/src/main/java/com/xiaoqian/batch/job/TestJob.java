97e1ae51f7230fa1d95b6d62e1bc0a0d110358e7
xiaoqian <1805521303@qq.com>
2025-04-22 22:27:58 +0800

    后端控制quartz任务并发执行

--- train12306-Java/batch/src/main/java/com/xiaoqian/batch/job/TestJob.java
+++ train12306-Java/batch/src/main/java/com/xiaoqian/batch/job/TestJob.java
@@ -1,13 +1,21 @@
 package com.xiaoqian.batch.job;
 
 import lombok.extern.slf4j.Slf4j;
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 
 @Slf4j
+@DisallowConcurrentExecution
 public class TestJob implements Job {
     @Override
     public void execute(JobExecutionContext jobExecutionContext)  {
-        log.info("TestJob");
+        log.info("TestJob开始执行");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        log.info("TestJob结束执行");
     }
 }
