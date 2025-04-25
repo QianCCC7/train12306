1264e05bc72d0a8c80a779d06be93fd286071b58
xiaoqian <1805521303@qq.com>
2025-04-25 16:46:55 +0800

    后端调整车次类型和车座类型，分别增加票价比例和基础票价属性

--- train12306-Java/business/src/main/java/com/xiaoqian/business/enums/SeatTypeEnum.java
+++ train12306-Java/business/src/main/java/com/xiaoqian/business/enums/SeatTypeEnum.java
@@ -6,18 +6,24 @@ import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.math.BigDecimal;
+
 @AllArgsConstructor
 @Getter
 public enum SeatTypeEnum {
-    YDZ("1", "一等座"),
-    EDZ("2", "二等座"),
-    RW("3", "软卧"),
-    YW("4", "硬卧")
+    YDZ("1", "一等座", new BigDecimal("0.4")),
+    EDZ("2", "二等座", new BigDecimal("0.3")),
+    RW("3", "软卧", new BigDecimal("0.6")),
+    YW("4", "硬卧", new BigDecimal("0.5"))
     ;
     @EnumValue
     private final String code;
     @JsonValue
     private final String desc;
+    /**
+     * 基础票价 N元/公里，0.4即为0.4元/公里
+     */
+    private final BigDecimal price;
 
     @JsonCreator(mode = JsonCreator.Mode.DELEGATING)
     public static SeatTypeEnum fromCode(String code) {
--- train12306-Java/business/src/main/java/com/xiaoqian/business/enums/TrainTypeEnum.java
+++ train12306-Java/business/src/main/java/com/xiaoqian/business/enums/TrainTypeEnum.java
@@ -6,19 +6,25 @@ import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.math.BigDecimal;
+
 
 @AllArgsConstructor
 @Getter
 public enum TrainTypeEnum {
 
-    G("G", "高铁"),
-    D("D", "动车"),
-    K("K", "快速");
+    G("G", "高铁", new BigDecimal("1.2")),
+    D("D", "动车", new BigDecimal("1.0")),
+    K("K", "快速", new BigDecimal("0.8"));
 
     @EnumValue
     private final String code;
     @JsonValue
     private final String desc;
+    /**
+     * 票价比例：比如1.1 => 则票价为 1.1 * 每公里单价（SeatTypeEnum.price） * 里程(trainStation.km)
+     */
+    private final BigDecimal priceRate;
 
     @JsonCreator(mode = JsonCreator.Mode.DELEGATING)
     public static TrainTypeEnum fromCode(String code) {
