b479cc955ae4f1f200c5939bcd71c6e0e04fcea5
xiaoqian <1805521303@qq.com>
2025-04-30 16:55:44 +0800

    后端business模块增加拦截器，存储会员信息

---
+++ train12306-Java/business/src/main/java/com/xiaoqian/business/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.xiaoqian.business.config;
+
+import com.xiaoqian.common.interceptor.MemberInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new MemberInterceptor())
+                .addPathPatterns("/**");
+    }
+}
